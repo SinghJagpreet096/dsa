5f58a2a75524a29f585d6bb38523831b0aaa582f
JAGPREET SINGH <singhjagpreet096@gmail.com>
2023-09-16 07:13:35 -0400

    ques 18 optimal sol

--- 18-4Sum.py
+++ 18-4Sum.py
@@ -8,21 +8,38 @@ all the unique quadruplets
 ## check if the position each element is unique2 are unique and sum == target
 ## timecomplexity : O(n**4)
 
+# 
+
 class Solution:
-    def fourSum(self, nums:list[int], target: int) -> list[list[int]]:
-        result = set()
+    def fourSum(self, nums:list[int],target:int) -> list[list[int]]:
+        res = []
+        nums.sort()
         n = len(nums)
         for i in range(n-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
             for j in range(i+1,n-2):
-                for k in range(j+1,n-1):
-                    for l in range(k+1,n):
-                        if (i != j and j!= k and k!=l) :
-                            if nums[i] + nums[j] + nums[k] + nums[l] == target:
-                                result.add(tuple(sorted([nums[i],nums[j],nums[k],nums[l]])))
-                                   
-           
-        return [list(res) for res in result]
-    
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                lo = j+1
+                hi = n-1
+                while lo < hi:
+                    # print(i,j,lo,hi)
+                    temp = nums[i] + nums[j] + nums[lo] + nums[hi]
+                    if temp == target:
+                        res.append([nums[i],nums[j],nums[lo],nums[hi]])
+                        while lo < hi and nums[lo]==nums[lo+1]:
+                            lo += 1
+                        lo += 1
+                        while lo<hi and nums[hi]==nums[hi-1]:
+                            hi -= 1
+                        hi -= 1
+                    elif temp < target:
+                        lo += 1
+                    else:
+                        hi -= 1
+        return res
+                
 s = Solution()
 print(s.fourSum([1,0,-1,0,-2,2],0))
 print(s.fourSum([-5,5,4,-3,0,0,4,-2],4))
